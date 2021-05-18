5b1e43fc6ad12a16a4162155c0603886b6ff144e
Melvin Sureshbabu <melvin.sureshbabu@gmail.com>
2021-05-18 15:13:59 -0500

    initial setup

---
+++ js/SceneMain.js
@@ -0,0 +1,19 @@
+class SceneMain extends Phaser.Scene {
+    constructor() {
+        super('SceneMain');
+    }
+    preload()
+    {
+    	//load our images or sounds here
+    }
+    create() {
+        //defines objects here
+        console.log('Ready!')
+    }
+    update() 
+    {
+        //constant running loop
+    }
+
+    //custom functions here
+}
